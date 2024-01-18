981506673524e10dc06675fcd7774578a2b016c3
vadym.mulish <vadym.mulish@pm.bet>
2024-01-18 22:14:11 +0200

    migrate some of the models and tools to the submodule project [1]

---
+++ Contracts/DataModels/Constants/RoleNames.cs
@@ -0,0 +1,8 @@
+﻿namespace Contracts.DataModels.Constants;
+
+public static class RoleNames
+{
+    public const string Administrator = "admin";
+    public const string Customer = "customer";
+    public const string Worker = "worker";
+}
