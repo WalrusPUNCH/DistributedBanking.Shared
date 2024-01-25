f608c72c0a92db72b9d3a3e895e03b65f4316829
vadym.mulish <vadym.mulish@pm.bet>
2024-01-25 20:49:18 +0200

    Remove unused classes

--- Contracts/DataModels/Constants/RoleNames.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Contracts.DataModels.Constants;
-
-public static class RoleNames
-{
-    public const string Administrator = "admin";
-    public const string Customer = "customer";
-    public const string Worker = "worker";
-}
