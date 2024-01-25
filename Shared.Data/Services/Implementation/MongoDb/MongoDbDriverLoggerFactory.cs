2af428816cc7c7612fa7e36ecfeb087a9ad94d8c
vadym.mulish <vadym.mulish@pm.bet>
2024-01-25 20:59:39 +0200

    add data access services

---
+++ Shared.Data/Services/Implementation/MongoDb/MongoDbDriverLoggerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Shared.Data.Services.Implementation.MongoDb;
+
+public static class MongoDbDriverLoggerFactory
+{
+    private static ILoggerFactory? _factory;
+    public static ILoggerFactory LoggerFactory
+    {
+        get
+        {
+            if (_factory != null)
+            {
+                return _factory;
+            }
+            
+            _factory = ConfigureLogger();
+            
+            return _factory;
+        }
+    }
+
+    private static ILoggerFactory ConfigureLogger()
+    {
+        return Microsoft.Extensions.Logging.LoggerFactory.Create(builder =>
+            builder.AddSimpleConsole(options =>
+            {
+                options.IncludeScopes = true;
+                options.SingleLine = true;
+                options.TimestampFormat = "HH:mm:ss ";
+            }));
+    }
+}
---
+++ Shared.Data/Services/Implementation/MongoDb/MongoDbFactory.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Logging;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core.Events;
+
+namespace Shared.Data.Services.Implementation.MongoDb;
+
+public class MongoDbFactory : IMongoDbFactory
+{
+    private readonly string _databaseName;
+    private readonly IMongoClient _client;
+
+    public MongoDbFactory(string connectionString, string databaseName)
+    {
+        _databaseName = databaseName;
+        
+        var settings = MongoClientSettings.FromConnectionString(connectionString);
+        settings.ServerApi = new ServerApi(ServerApiVersion.V1);
+#if DEBUG
+        settings.ClusterConfigurator = cb => { 
+            cb.Subscribe<CommandStartedEvent>(e => 
+            {
+                new Logger<MongoDbFactory>(MongoDbDriverLoggerFactory.LoggerFactory)
+                    .LogInformation("{CommandName} - {CommandJson}", e.CommandName, e.Command.ToJson());
+            });
+        };
+#endif
+
+        _client = new MongoClient(settings);
+    }
+    
+    public IMongoDatabase GetDatabase()
+    {
+        return _client.GetDatabase(_databaseName);
+    }
+
+    public IMongoClient GetClient()
+    {
+        return _client;
+    }
+}
