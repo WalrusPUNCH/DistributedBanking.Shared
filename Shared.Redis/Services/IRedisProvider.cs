ff36fda6e51f28c14945a749aacbb6381390eff8
vadym.mulish <vadym.mulish@pm.bet>
2024-02-07 21:02:33 +0200

    add Redis

---
+++ Shared.Redis/Services/IRedisProvider.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json.Serialization;
+
+namespace Shared.Redis.Services;
+
+public interface IRedisProvider
+{
+    Task<T?> GetAsync<T>(string key);
+    
+    Task SetAsync<T>(string key, T value, TimeSpan ttl);
+
+    Task SetAsync<T>(string key, T value, TimeSpan ttl, IContractResolver contractResolver);
+
+    Task SetManyAsync<T>(IReadOnlyDictionary<string, T> values, TimeSpan ttl);
+
+    Task<long> DeleteManyAsync(IEnumerable<string> keys);
+    
+    Task<bool> DeleteAsync(string key);
+}
