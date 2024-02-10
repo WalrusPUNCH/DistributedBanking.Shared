631dd05fa5f6efa6ace0b4c97693fe07d13494ff
vadym.mulish <vadym.mulish@pm.bet>
2024-02-10 23:16:31 +0200

    add IdentityOperationResult class

---
+++ Contracts/Models/IdentityOperationResult.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Contracts.Models;
+
+public class IdentityOperationResult
+{
+    public bool Succeeded { get; }
+    public IEnumerable<string> Errors { get; }
+
+    private IdentityOperationResult(bool success, IEnumerable<string>? errors = null)
+    {
+        Succeeded = success;
+        Errors = errors ?? new List<string>();
+    }
+
+    public static IdentityOperationResult Success { get; } = new(true);
+    public static IdentityOperationResult Failed(params string[] errors)
+    {
+        return new IdentityOperationResult(false, errors);
+    }
+    
+    public override string ToString()
+    {
+        return Succeeded ? 
+            "Succeeded" 
+            : string.Format(CultureInfo.InvariantCulture, "{0} : {1}", "Failed", string.Join(",", Errors.ToList()));
+    }
+}
