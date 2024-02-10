bba1047ef43bec51787e73e9e7cf375c3eed6409
vadym.mulish <vadym.mulish@pm.bet>
2024-02-10 22:24:13 +0200

    move Redis channel names to the Messaging project

---
+++ Shared.Messaging/Channel.cs
@@ -0,0 +1,12 @@
+﻿namespace Shared.Messaging;
+
+public static class Channel
+{
+    public const string CustomersRegistration = "customers:registration";
+    public const string WorkersRegistration = "workers:registration";
+    public const string CustomersUpdate = "customers:update";
+    public const string UsersDeletion = "users:deletion";
+    public const string AccountCreation = "accounts:creation";
+    public const string AccountDeletion = "accounts:deletion";
+    public const string TransactionsCreation = "transactions:creation";
+}
