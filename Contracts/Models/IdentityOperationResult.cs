52c65763e6b7275a24ee41406f38046c3e3ecedd
vadym.mulish <vadym.mulish@pm.bet>
2024-02-12 22:38:01 +0200

    update generic operation result entities

--- Contracts/Models/IdentityOperationResult.cs
+++ Contracts/Models/IdentityOperationResult.cs
@@ -14,10 +14,17 @@ public class IdentityOperationResult
     }
 
     public static IdentityOperationResult Success { get; } = new(true);
+    // todo differentiate between 500 and 400 response codes
+    // todo throw exception when 500
+    public static IdentityOperationResult BadRequest(params string[] errors)
+    {
+        return new IdentityOperationResult(false, errors);
+    }
     public static IdentityOperationResult Failed(params string[] errors)
     {
         return new IdentityOperationResult(false, errors);
     }
+    public static IdentityOperationResult Processing { get; } = new(true, new[] {"Your operation is still processing. Check for it results later"});
     
     public override string ToString()
     {
