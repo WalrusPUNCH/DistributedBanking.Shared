c9697098a4e3a1c21a4fb900c00a5ebd7c82619d
vadym.mulish <vadym.mulish@pm.bet>
2024-02-15 23:01:04 +0200

    add BsonElement attributes to the mongodb entities

--- Shared.Data/Entities/EndUsers/CustomerPassport.cs
+++ Shared.Data/Entities/EndUsers/CustomerPassport.cs
@@ -1,9 +1,18 @@
-﻿namespace Shared.Data.Entities.EndUsers;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Shared.Data.Entities.EndUsers;
 
 public class CustomerPassport
 {
+    [BsonElement(nameof(DocumentNumber))]
     public required string DocumentNumber { get; set; }
+    
+    [BsonElement(nameof(Issuer))]
     public required string Issuer { get; set; }
+
+    [BsonElement(nameof(IssueDateTime))]
     public required DateTime IssueDateTime { get; set; }
+
+    [BsonElement(nameof(ExpirationDateTime))]
     public required DateTime ExpirationDateTime { get; set; }
 }
--- Shared.Data/Entities/EndUsers/WorkerEntity.cs
+++ Shared.Data/Entities/EndUsers/WorkerEntity.cs
@@ -1,23 +1,33 @@
-﻿namespace Shared.Data.Entities.EndUsers;
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace Shared.Data.Entities.EndUsers;
 
 public class WorkerEntity : EndUserEntityBase
 {
+    [BsonElement(nameof(Position))]
     public required string Position { get; set; }
-    
+
+    [BsonElement(nameof(Address))]
     public required Address Address { get; set; }
 }
 
 public class Address
 {
+    [BsonElement(nameof(Country))]
     public required string Country { get; set; }
-    
+
+    [BsonElement(nameof(Region))]
     public required string Region { get; set; }
-    
+
+    [BsonElement(nameof(City))]
     public required string City { get; set; }
-    
+
+    [BsonElement(nameof(Street))]
     public required string Street { get; set; }
-    
+
+    [BsonElement(nameof(Building))]
     public required string Building { get; set; }
-    
+
+    [BsonElement(nameof(PostalCode))]
     public required string PostalCode { get; set; }
 }
